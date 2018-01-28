010c43a7a26cde040b22d772f0a5fb5635e3b3d4
Tom Carter <heymrcarter14@gmail.com>
2018-01-27 19:25:49 -0500

    update client id

--- config/prod.env.js
+++ config/prod.env.js
@@ -5,7 +5,7 @@ module.exports = {
   API_BASE_URL: '"https://www.bungie.net/Platform"',
   CLAN_ID: '"1789424"',
   AUTH_ENDPOINT: '"https://www.bungie.net/en/OAuth/Authorize"',
-  CLIENT_ID: '"23346"',
+  CLIENT_ID: '"23387"',
   OAUTH_SECRET: '"will you be my destiny dad?"',
   CLIENT_SECRET: '"GsLBFDR6D597L9TobHRNrOf6XMHZJfdzURv3cci9Uc4"'
 }
