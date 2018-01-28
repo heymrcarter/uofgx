0ccf08f46366324ca48e96af4efdb8570b6dc3a6
Tom Carter <heymrcarter14@gmail.com>
2018-01-28 09:27:56 -0500

    add tokens for sensitive env variables

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_KEY: '"da8cb399831b4b698dbd6aa4446b2eb5"',
+  API_KEY: '"#{API_KEY}#"',
   API_BASE_URL: '"https://www.bungie.net/Platform"',
   CLAN_ID: '"1789424"',
   AUTH_ENDPOINT: '"https://www.bungie.net/en/OAuth/Authorize"',
-  CLIENT_ID: '"23387"',
-  OAUTH_SECRET: '"will you be my destiny dad?"',
-  CLIENT_SECRET: '"GsLBFDR6D597L9TobHRNrOf6XMHZJfdzURv3cci9Uc4"'
+  CLIENT_ID: '"#{OAUTH_CLIENT_ID}#"',
+  OAUTH_SECRET: '"#{OAUTH_SECRET}#"',
+  CLIENT_SECRET: '"#{OAUTH_CLIENT_SECRET}#"'
 }
