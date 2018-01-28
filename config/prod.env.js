0eb29e915c07d7df2a96b873578253e7fdd64237
Tom Carter <heymrcarter14@gmail.com>
2018-01-27 19:14:06 -0500

    update prod env config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,11 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_KEY: '"076f6b2b8b4541eda553476e9379f7e4"',
+  API_BASE_URL: '"https://www.bungie.net/Platform"',
+  CLAN_ID: '"1789424"',
+  AUTH_ENDPOINT: '"https://www.bungie.net/en/OAuth/Authorize"',
+  CLIENT_ID: '"23346"',
+  OAUTH_SECRET: '"will you be my destiny dad?"',
+  CLIENT_SECRET: '"GsLBFDR6D597L9TobHRNrOf6XMHZJfdzURv3cci9Uc4"'
 }
