dbc21dce4a13efe96fa0ef833a07a4bc8d3fbe1c
Tom Carter <heymrcarter14@gmail.com>
2018-01-27 19:22:55 -0500

    updatea api key

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_KEY: '"076f6b2b8b4541eda553476e9379f7e4"',
+  API_KEY: '"da8cb399831b4b698dbd6aa4446b2eb5"',
   API_BASE_URL: '"https://www.bungie.net/Platform"',
   CLAN_ID: '"1789424"',
   AUTH_ENDPOINT: '"https://www.bungie.net/en/OAuth/Authorize"',
