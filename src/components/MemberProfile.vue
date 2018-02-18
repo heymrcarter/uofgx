c4ccd019e5de98cf5a878fae2ff497ba14ea4e30
Tom Carter <heymrcarter14@gmail.com>
2018-02-17 21:09:40 -0500

    add character selector

--- src/components/MemberProfile.vue
+++ src/components/MemberProfile.vue
@@ -10,21 +10,62 @@
       </v-flex>
     </v-layout>
 
-    <activity-history></activity-history>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <character-selector @characterChanged="characterChange"></character-selector>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
+      <v-flex xs12 md6 fill-height>
+        <activity-breakdown-chart :characterId="selectedCharacter" :membershipId="currentMembershipId"></activity-breakdown-chart>
+      </v-flex>
+
+      <v-flex xs12 md6>
+        <activity-by-date-chart :characterId="selectedCharacter" :membershipId="currentMembershipId"></activity-by-date-chart>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
+      <v-flex xs12>
+        <recent-activity-table :characterId="selectedCharacter" :membershipId="currentMembershipId"></recent-activity-table>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 import Characters from './profile/Characters'
 import Exemptions from './profile/Exemptions'
-import ActivityHistory from './profile/ActivityHistory'
+import CharacterSelector from './profile/CharacterSelector'
+import ActivityBreakdownChart from './profile/ActivityBreakdownChart'
+import ActivityByDateChart from './profile/ActivityByDateChart'
+import RecentActivityTable from './profile/RecentActivityTable'
 
 export default {
   name: 'member-profile',
   components: {
     Characters,
     Exemptions,
-    ActivityHistory
+    CharacterSelector,
+    ActivityBreakdownChart,
+    ActivityByDateChart,
+    RecentActivityTable
+  },
+  data() {
+    return {
+      selectedCharacter: undefined
+    }
+  },
+  computed: {
+    currentMembershipId() {
+      return this.$route.params.membershipId
+    }
+  },
+  methods: {
+    characterChange(nextCharacterId) {
+      this.selectedCharacter = nextCharacterId
+    }
   }
 }
 </script>
---
+++ src/components/profile/CharacterSelector.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card class="activity-card">
+    <v-card-title>
+      <v-flex xs12 md9>
+        <p class="headline">Character activity <span v-if="selectedCharacter !== undefined">for {{selectedCharacterClass}} ({{ selectedCharacterLight }})</span></p>
+      </v-flex>
+
+      <v-flex xs12 md3>
+        <v-select
+          color="yellow"
+          label="Select character"
+          :loading="isLoadingCharacters ? 'yellow' : false"
+          :items="characterDropdownOptions"
+          :value="selectedCharacter"
+          @change="selectedCharacterChanged"></v-select>
+      </v-flex>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'character-selector',
+  data() {
+    return {
+      selectedCharacter: undefined,
+      isLoadingCharacters: false
+    }
+  },
+  computed: {
+    ...mapGetters(['activeMember']),
+    selectedCharacterClass() {
+      return this.activeMember.characters.find(c => c.characterId === this.selectedCharacter).class
+    },
+    selectedCharacterLight() {
+      return this.activeMember.characters.find(c => c.characterId === this.selectedCharacter).light
+    },
+    characterDropdownOptions() {
+      if (this.activeMember) {
+        this.isLoadingCharacters = false // eslint-disable-line
+        return this.activeMember.characters.map((character, i) => {
+          return {
+            text: `${character.class} (${character.light})`,
+            value: character.characterId
+          }
+        })
+      } else {
+        this.isLoadingCharacters = true // eslint-disable-line
+        return []
+      }
+    }
+  },
+  methods: {
+    selectedCharacterChanged(characterId) {
+      this.selectedCharacter = characterId
+      this.$emit('characterChanged', characterId)
+    }
+  }
+}
+</script>
