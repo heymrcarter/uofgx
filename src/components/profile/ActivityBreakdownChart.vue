1a44faed22e1bdfaac76d3050d576ff2bbedbf81
Tom Carter <heymrcarter14@gmail.com>
2018-03-11 19:35:52 -0400

    refactor activity breakdown card into loadable card

--- src/components/profile/ActivityBreakdownChart.vue
+++ src/components/profile/ActivityBreakdownChart.vue
@@ -4,21 +4,29 @@
       <p class="title">Activity breakdown</p>
     </v-card-title>
     <v-card-text>
-      <pie-chart :data="chartData" :colors="chartColors" :library="chartOptions" v-if="chartData.length > 0 && !isLoading"></pie-chart>
-      <div class="loader" v-else-if="isLoading">
-        <p class="sr-only">Loading</p>
-        <v-progress-circular color="yellow" :size="100" indeterminate></v-progress-circular>
-      </div>
+      <loadable-indicator v-if="isLoading"></loadable-indicator>
+      <loadable-failure
+        v-else-if="loadError"
+        :message="`Couldn't load activity breakdown`"
+        :retryable="true"
+        @retry="fetch(characterId)"></loadable-failure>
 
+      <pie-chart :data="chartData" :colors="chartColors" :library="chartOptions" v-if="!isLoading && !loadError && chartData.length > 0"></pie-chart>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import LoadableIndicator from '@/components/LoadableIndicator'
+import LoadableFailure from '@/components/LoadableFailure'
 import { mapActions } from 'vuex'
 import colors from 'vuetify/es5/util/colors'
 export default {
   name: 'activity-breakdown-chart',
+  components: {
+    LoadableIndicator,
+    LoadableFailure
+  },
   props: {
     membershipId: {
       required: true,
@@ -32,6 +40,7 @@ export default {
   data() {
     return {
       isLoading: false,
+      loadError: false,
       chartColors: [colors.red.darken4, colors.blue.base, colors.indigo.base, colors.green.base, colors.yellow.base, colors.amber.base, colors.deepOrange.base, colors.pink.base],
       chartOptions: {
         segmentStrokeWidth: 20,
@@ -54,42 +63,26 @@ export default {
   watch: {
     characterId(nextCharacterId) {
       if (nextCharacterId) {
-        this.isLoading = true
-        this.getRecentActivityBreakdown({ membershipId: this.membershipId, characterId: nextCharacterId })
-          .then(recentActivityBreakdown => {
-            this.chartData = recentActivityBreakdown
-            this.isLoading = false
-          })
-          .catch(error => {
-            this.isLoading = false
-            console.error(error)
-          })
+        this.fetch(nextCharacterId)
       }
     }
   },
   methods: {
-    ...mapActions(['getRecentActivityBreakdown'])
+    ...mapActions(['getRecentActivityBreakdown']),
+    fetch(characterId) {
+      this.isLoading = true
+      this.loadError = false
+      this.getRecentActivityBreakdown({ membershipId: this.membershipId, characterId })
+        .then(recentActivityBreakdown => {
+          this.chartData = recentActivityBreakdown
+          this.isLoading = false
+        })
+        .catch(error => {
+          this.isLoading = false
+          this.loadError = true
+          console.error(error)
+        })
+    }
   }
 }
 </script>
-
-<style scoped>
-.sr-only {
-  border: 0;
-  clip: rect(1px, 1px, 1px, 1px);
-  clip-path: inset(50%);
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute !important;
-  width: 1px;
-  word-wrap: normal !important;
-}
-
-.loader {
-  display: flex;
-  justify-content: center;
-  height: 100%;
-}
-</style>
