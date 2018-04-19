02b2c45ce56f9dcfd5bc7ade1cae360e43073c6b
Tom Carter <heymrcarter14@gmail.com>
2018-04-19 13:00:38 -0400

    get aws creds from environment

--- update-index-document.js
+++ update-index-document.js
@@ -1,5 +1,11 @@
 const AWS = require('aws-sdk')
-const s3 = new AWS.S3()
+
+const opts = {
+  accessKeyId: process.env.AWS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+}
+
+const s3 = new AWS.S3(opts)
 
 var params = {
   Bucket: 'destinyclanmanager',
