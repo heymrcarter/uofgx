68e445df61157f72a214c7e7877843584ed9c420
Tom Carter <heymrcarter14@gmail.com>
2018-08-11 18:24:46 -0400

    fixes text truncation in clan overview on mobile

--- src/components/dashboard/ClanOverviewItem.vue
+++ src/components/dashboard/ClanOverviewItem.vue
@@ -4,12 +4,13 @@
       <v-icon>{{ icon }}</v-icon>
     </v-list-tile-avatar>
 
-    <v-list-tile-content>
+    <v-list-tile-content @click="triggerAction">
       <v-list-tile-title>{{ text }}</v-list-tile-title>
     </v-list-tile-content>
 
     <v-list-tile-action v-if="actionText">
-      <v-btn flat @click="$emit('action')">{{ actionText }}</v-btn>
+      <v-btn v-responsive.xs flat @click="$emit('action')" icon><v-icon>{{ mobileButtonIcon }}</v-icon></v-btn>
+      <v-btn v-responsive.sm.md.lg.xl flat @click="$emit('action')">{{ actionText }}</v-btn>
     </v-list-tile-action>
   </v-list-tile>
 </template>
@@ -29,6 +30,22 @@ export default {
     text: {
       type: String,
       require: true
+    }
+  },
+  computed: {
+    mobileButtonIcon() {
+      if (this.actionText === 'Reload' || this.actionText === 'Retry') {
+        return 'refresh'
+      }
+
+      return 'arrow_forward'
+    }
+  },
+  methods: {
+    triggerAction() {
+      if (this.$vuetify.breakpoint.name === 'xs') {
+        this.$emit('action')
+      }
     }
   }
 }
