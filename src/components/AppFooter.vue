88afc5a427b775e5c10c20f9f760382022a83078
Tom Carter <heymrcarter14@gmail.com>
2018-08-12 14:55:13 -0400

    fix footer alignment on mobile

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,20 +1,50 @@
 <template>
-  <v-footer height="60px" class="pa-2">
-    <v-layout row align-center>
-      <v-flex flex="1">
-        &copy; {{ new Date().getFullYear() }} - Tom Carter
-      </v-flex>
+  <v-footer class="pa-2">
+    <v-container v-responsive.xs class="pa-0">
+      <v-layout row align-center>
+        <v-flex class="legal">
+          &copy; {{ new Date().getFullYear() }} - Tom Carter
+        </v-flex>
 
-      <div>
-        <v-btn flat to="/roadmap">Roadmap</v-btn>
-      </div>
-      <div v-if="hasAccess">
-        <v-btn flat to="/suggest-feature">Suggest a feature</v-btn>
-      </div>
-      <div>
-        <v-btn flat icon href="https://twitter.com/destinyclanmgr"><v-icon size="24px">fab fa-twitter</v-icon></v-btn>
-      </div>
-    </v-layout>
+        <v-spacer></v-spacer>
+
+        <v-flex>
+          <v-btn flat icon href="https://twitter.com/destinyclanmgr"><v-icon size="24px">fab fa-twitter</v-icon></v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row>
+        <v-flex xs-6>
+          <v-btn flat to="/roadmap" block>Roadmap</v-btn>
+        </v-flex>
+
+        <v-flex v-if="hasAccess" xs-6>
+          <v-btn flat to="/suggest-feature" block>Suggest a feature</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container v-responsive.sm.md.lg.xl class="pa-0">
+      <v-layout row>
+        <v-flex class="legal">
+          &copy; {{ new Date().getFullYear() }} - Tom Carter
+        </v-flex>
+
+        <v-spacer></v-spacer>
+
+        <v-flex>
+          <v-btn flat to="/roadmap">Roadmap</v-btn>
+        </v-flex>
+
+        <v-flex v-if="hasAccess">
+          <v-btn flat to="/suggest-feature">Suggest a feature</v-btn>
+        </v-flex>
+
+        <v-flex>
+          <v-btn flat icon href="https://twitter.com/destinyclanmgr"><v-icon size="24px">fab fa-twitter</v-icon></v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-footer>
 </template>
 
@@ -27,3 +57,19 @@ export default {
   }
 }
 </script>
+
+<style>
+.v-footer {
+  height: auto !important;
+}
+</style>
+
+<style scoped lang="scss">
+.legal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  flex-direction: column;
+}
+</style>
